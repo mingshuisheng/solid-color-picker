3b8e4153b660a37641c0197a35c4daa6a2be0937
mingshuisheng <1820924213@qq.com>
2023-12-23 12:33:23 +0800

    feat: add function hsvToHex and hexToHsv

--- src/libs.ts
+++ src/libs.ts
@@ -1,1 +1,11 @@
+import tinycolor from "tinycolor2";
+
 export * from "./components";
+
+export function hsvToHex(hsv: tinycolor.ColorFormats.HSL) {
+  return tinycolor(hsv).toHex();
+}
+
+export function hexToHsv(hex: string) {
+  return tinycolor(hex).toHsv();
+}
